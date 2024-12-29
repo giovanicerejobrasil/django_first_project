df3e82a0de7211451fb7792283dfde675ac6cf98
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-12-29 16:35:38 -0300

    update: utilizando a mesma url de forma estática e dinâmica

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    # url estática e dinâmica
+    # path('post/', views.blog, name='blog'),
+    # path('post/<int:id>/', views.post, name='post'),
+
     path('', views.blog, name='blog'),
-    path('post/<int:id>', views.post, name='post'),
+    path('post/<int:id>/', views.post, name='post'),
     path('example/', views.example, name='example'),
 ]
