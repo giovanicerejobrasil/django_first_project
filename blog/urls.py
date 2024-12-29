50d2611a6c8f78fdcbaa89305c2a05504e43715d
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-12-29 14:14:01 -0300

    update: urls dinâmicas no urls.py e nos templates html

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.blog),
-    path('example/', views.example)
+    path('', views.blog, name='blog'),
+    path('example/', views.example, name='example')
 ]
